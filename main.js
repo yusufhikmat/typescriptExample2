b67923d21da2e20d6ada99733c92f717ad7af5b1
yusufhikmat <hikmatyusuff2020@gmail.com>
2023-02-12 22:39:35 -1200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+//types
+var message = "hello ayo";
+console.log(message);
+var total = 0;
+console.log(total);
+var name = "Ayo";
+var multiType;
+multiType = 20;
+multiType = true;
+var anyType;
+anyType = "Ayo";
+anyType = 30;
+anyType = true;
+//functiions
---
+++ main.ts
@@ -0,0 +1,20 @@
+export {}
+//types
+const message = "hello ayo"
+console.log(message)
+
+let total:number = 0;
+console.log(total)
+let name:string = "Ayo";
+
+let multiType : number | boolean;
+
+multiType = 20;
+multiType = true;
+
+let anyType: any;
+anyType = "Ayo";
+anyType = 30;
+anyType = true;
+
+//functiions
